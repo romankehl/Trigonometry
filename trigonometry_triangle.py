7c7ee44a532510310c093497db654cb5f76725db
romankehl <roman.kehl@rsnweb.ch>
2022-10-30 23:14:45 +0100

    Alpha und Beta working

--- trigonometry_triangle.py
+++ trigonometry_triangle.py
@@ -1,9 +1,11 @@
 import math
 
+print("Die Angaben müssen aus Sicht von alpha geschen!")
+
 #Get Inputs
 while True: 
     try:
-        a = float(input("Ankathete: ").strip())
+        a = float(input("Ankathete: ").strip()) #Ankathete von Winkel alpha
     except ValueError:
         print("Ankathete muss eine Zahl sein!")
     else:
@@ -11,7 +13,7 @@ while True:
 
 while True:
     try:
-        g = float(input("Gegenkathete: ").strip())
+        g = float(input("Gegenkathete: ").strip()) #Gegenkathete von winkel alpha
     except ValueError:
         print("Gegenkathete muss eine Zahl sein!")
     else:
@@ -94,17 +96,81 @@ while True:
     if alpha > 0:
         break
     elif a > 0 and g > 0:
-        print(f"Alpha: {wa_ag()}°")
+        print(f"Alpha: {wa_ag(): .3f}°")
         break
     elif a > 0 and h > 0:
-        print(f"Alpha: {wa_ah()}°")
+        print(f"Alpha: {wa_ah(): .3f}°")
         break
     elif g > 0 and h > 0:
-        print(f"Alpha: {wa_gh()}°")
+        print(f"Alpha: {wa_gh(): .3f}°")
         break
     else:
-        print(f"Alpha: {wa_wb()}°")
+        print(f"Alpha: {wa_wb(): .3f}°")
         break
 
 
+#Ankathete und Gegenkathete müssen für Beta getauscht werden
 
+#Define functions for beta
+def wb_ag():
+    if a == 0 or g == 0:
+        return None
+    else:
+        while True:
+            try:
+                wbag = math.degrees(math.atan(a / g))
+                return wbag
+            except ZeroDivisionError:
+                wbag = None
+                break
+            else:
+                break
+
+def wb_ah():
+    if a == 0 or h == 0:
+        return None
+    else:
+        while True:
+            try:
+                wbah = math.degrees(math.acos(g / h))
+                return wbah
+            except ZeroDivisionError:
+                wbah = None
+                break
+            else:
+                break
+
+def wb_gh():
+    if g == 0 or h == 0:
+        return None
+    else:
+        while True:
+            try:
+                wbgh = math.degrees(math.asin(a / h))
+                return wbgh
+            except ZeroDivisionError:
+                wbgh = None
+                break
+            else:
+                break
+
+def wb_wa():
+    wbwb = math.degrees(math.radians(90) - alpha)
+    return wbwb
+
+#printing functions of beta
+while True:
+    if beta > 0:
+        break
+    elif a > 0 and g > 0:
+        print(f"Beta: {wb_ag(): .3f}°")
+        break
+    elif g > 0 and h > 0:
+        print(f"Beta: {wb_ah(): .3f}°")
+        break
+    elif a > 0 and h > 0:
+        print(f"Beta: {wb_gh(): .3f}°")
+        break
+    else:
+        print(f"Beta: {wb_wa(): .3f}°")
+        break
